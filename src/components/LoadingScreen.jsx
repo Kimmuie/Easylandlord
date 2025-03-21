9ade484854c9cfb6f66a53817f4472b87ddc4193
Kimmuie <pranchayutnetsawang@gmail.com>
2025-03-21 16:40:21 +0700

    Navbar and Loading Scene

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -6,44 +6,19 @@ const LoadingScreen = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(false);
-    }, 4000);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  const durationArray = [2000, 2050, 2100, 2150, 2200, 2250];
-  const randomIndex = Math.floor(Math.random() * durationOptions.length);
-  const columns = [
-    { id: 1, duration: `duration-${durationArray[randomIndex]}` },
-    { id: 2, duration: `duration-${durationArray[randomIndex]}` },
-    { id: 3, duration: `duration-${durationArray[randomIndex]}` },
-    { id: 4, duration: `duration-${durationArray[randomIndex]}` },
-    { id: 5, duration: `duration-${durationArray[randomIndex]}` },
-    { id: 6, duration: `duration-${durationArray[randomIndex]}` },
-    { id: 7, duration: `duration-${durationArray[randomIndex]}` },
-    { id: 8, duration: `duration-${durationArray[randomIndex]}` }
-  ];
-
   return (
-    <div className="fixed inset-0 z-40 overflow-hidden">
-      {/* GIF overlay on screen */}
-      <div 
-        className={`absolute inset-0 flex items-center justify-center z-50 transition-transform ${
-          isVisible ? "translate-y-0" : "-translate-y-full"
-        } duration-2000 ease-in-out`}>
-        <img src="./img/sign.gif" width="300" height="300" className="object-contain" alt="Loading..." />
-      </div>
-      {/* Stagger Animation */}
-      <div className="flex h-full w-full">
-        {columns.map((column) => (
-          <div
-            key={column.id}
-            className={`flex-1 bg-seconBlue1 transition-transform ${
-              isVisible ? "translate-y-0" : "-translate-y-full"
-            } ${column.duration} ease-in-out`}
-          ></div>
-        ))}
-      </div>
+    <div
+      className={`absolute flex flex-col w-full h-screen items-center justify-center z-50 
+        bg-ellBlack transition-all duration-1000 ease-in-out
+        ${isVisible ? "translate-y-0" : "-translate-y-full"}
+      `}>
+      <img src="./img/wHome.svg" width="90" height="90" alt="Loading..." />
+      <div className="text-ellWhite text-lg font-inter font-extrabold">Easylandlord</div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [active, setActive] = useState("บริหาร");
+  const menuItems = ["บริหาร", "การเงิน", "บ้านเช่า", "บัญชี"];
+
   return (
-    <div className="relative flex w-full h-20 bg-primeWhite items-center px-4 drop-shadow-md">
-      <img src="./img/signBlue.png" width="140" height="140" alt="signLogo" />
-      <div className="flex-8 flex justify-end">
-        <button className="text-primeBlack text-lg font-ibarra font-semibold">About</button>
-        <button className="text-primeBlack text-lg font-ibarra font-semibold">Experience</button>
-        <button className="text-primeBlack text-lg font-ibarra font-semibold">Project</button>
-        <button className="text-primeBlack text-lg font-ibarra font-semibold">Contact</button>
+    <div className="relative flex w-full h-20 bg-ellWhite items-center drop-shadow-md">
+      <div className="flex w-full items-center justify-between pl-6 pr-6">
+        <div className="flex flex-row items-center gap-2">
+          <img src="./img/bHome.svg" width="40" height="40" alt="logoEasylandlord" />
+          <div className="text-ellBlack text-lg font-inter font-semibold">Easylandlord</div>
+        </div>
+
+        {/* Navigation */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-6">
+          {menuItems.map((item) => (
+            <button
+              key={item}
+              className={`relative text-lg font-prompt font-semibold ${
+                active === item ? "text-ellBlack" : "text-ellBlack hover:text-ellRed cursor-pointer"
+              }`}
+              onClick={() => setActive(item)}
+            >
+              {item}
+              <div className="flex justify-center">
+              {active === item && <div className="absolute w-15 h-2 bg-ellBlack" style={{ marginTop: "18px"}}></div>}
+              </div>
+            </button>
+          ))}
+        </div>
+        {/* Notification */}
+        <div>
+          <button>
+          <img src="./img/notification.svg" width="40" height="40" alt="notifications" />
+          </button>
+        </div>
       </div>
-      <img src="./img/Sun.png" width="56" height="56"/>
     </div>
   );
 };
